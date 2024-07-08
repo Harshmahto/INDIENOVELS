083c59cace0678dd22462ee04e8bea0616551cde
Harshmahto <62500223+Harshmahto@users.noreply.github.com>
2024-07-08 16:12:06 +0530

    some chages

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,16 @@
+import asyncHandler from "../utils/asyncHandler";
+
+
+const registerUser = asyncHandler( async (req,res) => { 
+    res.status(200).json({
+        message:"ooooooook"
+    })
+})
+
+
+
+export 
+    { 
+        registerUser,
+        
+    }
